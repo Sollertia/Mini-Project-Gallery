619ee55a28a3cd01096401d1b51c1c5e019c8c38
woojin <cmshim22@gmail.com>
2021-10-11 20:07:13 +0900

    Dto 생성

---
+++ src/main/java/com/hanghae/gallery/dto/ArtistDto.java
@@ -0,0 +1,16 @@
+package com.hanghae.gallery.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ArtistDto {
+
+    private String username;
+    private String nickname;
+    private String password;
+
+}
