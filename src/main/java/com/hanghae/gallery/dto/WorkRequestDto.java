c1c035c86a7d66ed7504557855174844e3652c76
9sanha <64877575+9sanha@users.noreply.github.com>
2021-10-12 04:33:53 +0900

    Dto 업데이트

--- src/main/java/com/hanghae/gallery/dto/WorkRequestDto.java
+++ src/main/java/com/hanghae/gallery/dto/WorkRequestDto.java
@@ -6,8 +6,15 @@ import lombok.Setter;
 
 //클라이언트 -> 서버 받을 때
 @Getter
-@Setter
 @NoArgsConstructor
 public class WorkRequestDto {
+
+    private String image;
+    private String workTitle;
+    private String workDesc;
+    private String workSize;
+    private String workMaterial;
+    private String workMade;
+
 
 }
--- src/main/java/com/hanghae/gallery/dto/WorkResponseDto.java
+++ src/main/java/com/hanghae/gallery/dto/WorkResponseDto.java
@@ -1,5 +1,6 @@
 package com.hanghae.gallery.dto;
 
+import com.hanghae.gallery.model.Work;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,11 +12,23 @@ import lombok.Setter;
 public class WorkResponseDto {
 
     private Long id;
+    private String image;
     private String workTitle;
+    private String workDesc;
     private String workSize;
     private String workMaterial;
     private String workMade;
-    private String workDesc;
     private Long artistId;
-    private String image;
+
+    public WorkResponseDto(Work work){
+        this.id = work.getId();
+        this.artistId = work.getArtistId();
+        this.workDesc = work.getWorkDesc();
+        this.workMade = work.getWorkMade();
+        this.workSize = work.getWorkSize();
+        this.image = work.getImage();
+        this.workMaterial = work.getWorkMaterial();
+        this.workTitle = work.getWorkTitle();
+    }
+
 }
