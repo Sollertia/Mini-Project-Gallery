a14a10b0af64305d31116f9f88a3ac4aa6e5e0b6
sollertia <sollertia331@gmail.com>
2021-10-16 04:39:28 +0900

    다시 세팅

--- src/main/java/com/hanghae/gallery/GalleryApplication.java
+++ src/main/java/com/hanghae/gallery/GalleryApplication.java
@@ -2,11 +2,14 @@ package com.hanghae.gallery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GalleryApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(GalleryApplication.class, args);
     }
 
